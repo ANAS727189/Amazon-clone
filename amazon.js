979f0b48cd346333d715921aee6dfc66d36323ee
ANAS727189 <144107389+ANAS727189@users.noreply.github.com>
2023-11-04 19:31:42 +0530

    initial commit

---
+++ amazon.js
@@ -0,0 +1,15 @@
+
+var backToTopButton = document.querySelector(".foot-panel1");
+
+
+backToTopButton.addEventListener("click", function () {
+    // Scroll to the top of the page with smooth behavior
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+
+
